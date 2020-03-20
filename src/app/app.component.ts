aa2278abc0d5bc1732fa1d0323aca9ec56b05149
Mohammad Hossein Karami <mhkarami1997@gmail.com>
2020-03-20 15:50:16 +0330

    add user menu click and log out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 import { AnalyticsService } from './@core/utils/analytics.service';
 import { SeoService } from './@core/utils/seo.service';
 import { Title } from '@angular/platform-browser';
+import { NbMenuService } from '@nebular/theme';
+import { NbAuthService, NbAuthJWTToken, NbTokenService } from '@nebular/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ngx-app',
@@ -14,15 +17,36 @@ import { Title } from '@angular/platform-browser';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private analytics: AnalyticsService, private seoService: SeoService, private titleService: Title) {
+  constructor(private analytics: AnalyticsService,
+    private seoService: SeoService,
+    private titleService: Title,
+    private menuService: NbMenuService,
+    private authService: NbAuthService,
+    private tokenService: NbTokenService,
+    private router: Router) {
   }
 
   public setTitle(newTitle: string) {
     this.titleService.setTitle('پنل مدیریت | ' + newTitle);
   }
 
+  onContecxtItemSelection(title: string) {
+    if (title === 'خروج') {
+      this.tokenService.clear();
+      // this.authService.logout('');
+      this.router.navigate(['auth/login']);
+    } else if (title === 'پروفایل') {
+      console.log('Profile Clicked');
+    }
+  }
+
   ngOnInit(): void {
     this.analytics.trackPageViews();
     this.seoService.trackCanonicalChanges();
+
+    this.menuService.onItemClick()
+      .subscribe((event) => {
+        this.onContecxtItemSelection(event.item.title);
+      });
   }
 }
